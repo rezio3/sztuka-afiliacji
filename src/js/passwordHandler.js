e7663f2dec418a83f363f05b53066f2deca40d84
Joszko <jakub.rezler96@gmail.com>
2023-10-31 21:40:03 +0100

    fix password handler bug

--- src/js/passwordHandler.js
+++ src/js/passwordHandler.js
@@ -1,20 +1,15 @@
 const passwordForm = document.getElementsByClassName(
 	"password-section-container"
 );
-const passwordInput = document.querySelector(".input-password");
+const passwordInput = document.querySelector("#password");
 const wrongPasswordSpan = document.querySelector(".wrong-password-span");
-const okButton = document.querySelector(".check-password-button");
 const mainSection = document.querySelector(".main-section-container");
 
 let nextPage = false;
-let passInput = "";
 
-const inputChange = (e) => {
-	passInput = e.value;
-};
-console.log(okButton);
-const checkPassword = () => {
-	if (passInput === "PRODUKTY") {
+const checkPassword = (event) => {
+	event.preventDefault();
+	if (passwordInput.value === "PRODUKTY") {
 		nextPage = true;
 		passwordForm[0].classList.add("page-off");
 		mainSection.classList.remove("page-off");
@@ -26,6 +21,7 @@ const checkPassword = () => {
 
 passwordInput.addEventListener("keydown", (event) => {
 	if (event.key === "Enter") {
-		checkPassword();
+		event.preventDefault();
+		checkPassword(event);
 	}
 });
