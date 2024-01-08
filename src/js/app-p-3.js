c883d101ef8a34dce913237273b0db8a02d501b6
Joszko <jakub.rezler96@gmail.com>
2024-01-08 03:30:01 +0100

    update animation in app-p-3.js

--- src/js/app-p-3.js
+++ src/js/app-p-3.js
@@ -1,24 +1,37 @@
-const target = document.querySelector("#target");
+const target1 = document.querySelectorAll("#target1");
+const target2 = document.querySelectorAll("#target2");
+const target3 = document.querySelectorAll("#target3");
+const target4 = document.querySelectorAll("#target4");
+const target5 = document.querySelectorAll("#target5");
+const target6 = document.querySelectorAll("#target6");
+const target7 = document.querySelectorAll("#target7");
+const target8 = document.querySelectorAll("#target8");
+const target9 = document.querySelectorAll("#target9");
+
 gsap.registerPlugin(ScrollTrigger);
 
-// const handleIntersection = (entries, observer) => {
-// 	entries.forEach((entry) => {
-// 		console.log(entry.isIntersecting);
-// 	});
-// };
+const targetsArr = [
+	target1,
+	target2,
+	target3,
+	target4,
+	target5,
+	target6,
+	target7,
+	target8,
+	target9,
+];
 
-// const observer = new IntersectionObserver(handleIntersection);
-
-// observer.observe(target);
-
-gsap.from(target, {
-	scrollTrigger: {
-		trigger: target,
-		start: "top bottom",
-		end: "top 300px",
-		scrub: 1,
-		markers: true,
-	},
-	x: "200px",
-	opacity: 0,
+targetsArr.map((e) => {
+	gsap.from(e, {
+		scrollTrigger: {
+			trigger: e,
+			start: "top 80%",
+			end: "top 300px",
+			scrub: 1,
+			markers: true,
+		},
+		x: "200px",
+		opacity: 0,
+	});
 });
